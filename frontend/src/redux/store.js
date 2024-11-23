b7b7b9113a3ee608317d4a831e611a1c68d9117b
Victor Socinschi <chaosdaddy84@gmail.com>
2024-11-23 13:40:51 +0200

    Filter Redux slice and filter by title

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookReducer from "./books/reducer";
-
+import filterReducer from "./slices/filterSlice";
 const store = configureStore({
   reducer: {
     books: bookReducer,
+    filter: filterReducer,
   },
 });
 
